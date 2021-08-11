0670ae40b34209159a33039da7a35dd248ef02cc
Layna Oyervides <laynaoyervides@gmail.com>
2021-08-11 16:59:21 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 2;
+let num2= 31; 
+let multiply = num1 * num2;
 
+
+let random = Math.floor(Math.random()*10)+ 1;
+//return 
+let num3 = 19;
+let num4 = 5;
+let mod = num3 % num4;
+
+//var max = 20;
+let max = Math.max(10, 15, 20);
 // Write your code here
